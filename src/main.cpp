c01f9fc32d8e891dd829293789e64e15bad49920
Tristan Pippins <tristan.pippins@gmail.com>
2021-06-06 19:14:10 -0700

    saving

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <max6675.h>
-#include <timer.h>
+#include <Ticker.h>
 #include <PID_v1.h>
 #include <Nextion.h>
 
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
 //NOTES**********************************************************************************
 //  Currently on Version 2 of the nextion screen.
 //  Changes:
@@ -21,18 +24,21 @@
 #define KD 8.1915
 
 #define INC_TMP_BUTTON 3
-#define DEC_TMP_BUTTON 1
-#define REAL_TIME_TEMP_TEXTBOX 4
-#define SET_TEMP_TEXTBOX 5
+#define DEC_TMP_BUTTON 2
+#define SLEEP_MODE_ID 13
+#define REAL_TIME_TEMP_TEXTBOX 5
+#define SET_TEMP_TEXTBOX 4
 
 #define SET_INCREMENT 25
 #define MAX_TEMP_SETPOINT 1000
+#define SLEEP_TEMP 0
 
 #define MAIN_PAGE 0
 #define SETTING_PAGE 1
 #define CLEAN_MODE_PAGE 2
 #define BRIGHTNESS_PAGE 3
 
+#define GLOBAL_DEFAULT_SET_TEMP_SETPOINT 650
 
 //FUNCTION DECLERATION*******************************************************************
 void update(); //Execute timer function.
@@ -45,11 +51,14 @@ void clearBuffer();
 int GLOBAL_OLD_TEMP = 0;
 double GLOBAL_TEMP = 0;
 double GLOBAL_TEMP_SETPOINT = 650;
+double GLOBAL_TEMP_SAVEPOINT;
 double GLOBAL_TEMP_OUTPUT;
+bool SLEEP_STATUS = false;
 
 
 // Timers********************************************************************************
-Timer timer;
+Ticker ticker1(pidExecute, 220);
+Ticker ticker2(update, 500);
 
 // Thermocouple**************************************************************************
 MAX6675 thermocouple(THERMO_CLK, THERMO_CS, THERMO_DO);
@@ -64,12 +73,14 @@ NexText INC_TMP = NexText(MAIN_PAGE, INC_TMP_BUTTON, "INC_TMP");
 NexText DEC_TMP = NexText(MAIN_PAGE, DEC_TMP_BUTTON, "DEC_TMP");
 NexText REAL_TIME_TMP_TEXTBOX = NexText(MAIN_PAGE,REAL_TIME_TEMP_TEXTBOX,"Real_Time_TMP");
 NexText SET_TMP_TEXTBOX = NexText(MAIN_PAGE,SET_TEMP_TEXTBOX,"Set_Temp");
-
-char GLOBAL_BUFFER[10] = {0};
+NexVariable SLEEP_MODE = NexVariable(MAIN_PAGE,SLEEP_MODE_ID,"SLP");
+uint32_t number = 0;
+char GLOBAL_BUFFER[20] = {0};
 
 NexTouch *next_listen_list[] = {
   &INC_TMP,
   &DEC_TMP,
+  &SLEEP_MODE,
   NULL
 };
 
@@ -80,8 +91,6 @@ NexTouch *next_listen_list[] = {
 
 //SETUP***********************************************************************************
 void setup() {
-  //Nextion Init
-  nexInit();
 
   //Temp Sensor
   GLOBAL_TEMP = thermocouple.readFarenheit(); //Temp in F.
@@ -92,10 +101,12 @@ void setup() {
   //PID
   eNail.SetMode(AUTOMATIC);
 
+  // Timers
+  ticker1.start();
+  ticker2.start();
 
-  //Timers
-  timer.every(220,pidExecute);
-  timer.every(500,update);
+  //Nextion INIT
+  nexInit();
 
   //Nextion Button Callbacks
   INC_TMP.attachPush(inc_tmp_PushCallback);
@@ -104,7 +115,8 @@ void setup() {
 }
 //LOOP***********************************************************************************
 void loop() {
-  timer.update();
+  ticker1.update();
+  ticker2.update();
   nexLoop(next_listen_list);
 }
 
@@ -129,8 +141,22 @@ void dec_tmp_PushCallBack(void *ptr){
   SET_TMP_TEXTBOX.setText(GLOBAL_BUFFER);
 }
 
-void update(){ //Execute Timer Function.
-  int temp = floor(GLOBAL_TEMP);
+void update() { //Execute Timer Function.
+  int temp = 0;
+  SLEEP_MODE.getValue(&number);
+  
+  //Sleep Mode If statement.
+  if ((number == 1) and (SLEEP_STATUS == false)) {
+    GLOBAL_TEMP_SAVEPOINT = GLOBAL_TEMP_SETPOINT;
+    GLOBAL_TEMP_SETPOINT = SLEEP_TEMP;
+    SLEEP_STATUS = true;
+  }
+  else if (number == 0 and SLEEP_STATUS == true) {
+    GLOBAL_TEMP_SETPOINT = GLOBAL_TEMP_SAVEPOINT;
+    SLEEP_STATUS = false;
+  }
+  
+  temp = floor(GLOBAL_TEMP);
   clearBuffer();
   itoa(temp,GLOBAL_BUFFER,10);
   const char *F = " F";
@@ -144,7 +170,7 @@ void update(){ //Execute Timer Function.
   
 }
 
-void pidExecute(){
+void pidExecute() {
   GLOBAL_TEMP = thermocouple.readFarenheit(); //Temp in F
   eNail.Compute();
   analogWrite(SSR,GLOBAL_TEMP_OUTPUT);
